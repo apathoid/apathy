2db8388b3d476aa598bcd5bbc712275bc0ed3e18
collapsly <collapsly@protonmail.com>
2022-11-13 00:19:53 +0300

    neotree: mappings for winbar's windows were fixed

--- lua/stage2/plugins/neotree.lua
+++ lua/stage2/plugins/neotree.lua
@@ -53,16 +53,48 @@ neotree.setup({
         position = apth.config.file_explorer.position,
         width = apth.config.file_explorer.width,
         mappings = {
-            o = 'open',
-            H = 'prev_source',
-            L = 'next_source',
-            h = 'toggle_hidden'
+            ['o'] = 'open',
+            ['H'] = 'prev_source',
+            ['L'] = 'next_source'
         }
     },
     filesystem = {
         follow_current_file = true,
         hijack_netrw_behavior = 'open_default',
-        use_libuv_file_watcher = true
+        use_libuv_file_watcher = true,
+        window = {
+            mappings = {
+                ['<bs>'] = 'navigate_up',
+                ['.'] = 'set_root',
+                ['h'] = 'toggle_hidden',
+                ['/'] = 'fuzzy_finder',
+                ['D'] = 'fuzzy_finder_directory',
+                ['f'] = 'filter_on_submit',
+                ['<c-x>'] = 'clear_filter',
+                ['[g'] = 'prev_git_modified',
+                [']g'] = 'next_git_modified'
+            }
+        }
+    },
+    buffers = {
+        mappings = {
+            ['bd'] = 'buffer_delete',
+            ['<bs>'] = 'navigate_up',
+            ['.'] = 'set_root',
+        }
+    },
+    git_status = {
+        window = {
+            mappings = {
+                ['A']  = 'git_add_all',
+                ['gu'] = 'git_unstage_file',
+                ['ga'] = 'git_add_file',
+                ['gr'] = 'git_revert_file',
+                ['gc'] = 'git_commit',
+                ['gp'] = 'git_push',
+                ['gg'] = 'git_commit_and_push'
+            }
+        }
     },
     event_handlers = {}
 })
