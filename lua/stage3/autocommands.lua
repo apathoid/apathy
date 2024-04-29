84b7e2cc047fd3c1ff30046b6a5ef06c2b1bf2a1
apathoid <apathoid@tuta.io>
2024-04-29 17:04:09 +0300

    editor: open [no name] buffer if the neovim's argument is a path to a dir

--- lua/stage3/autocommands.lua
+++ lua/stage3/autocommands.lua
@@ -12,6 +12,15 @@ M.commands = {
                 local cwd = vim.fn.getcwd()
                 local arg_path = vim.fn.argv()[1] or ''
                 local path = vim.fn.expand(cwd..'/'..arg_path)
+                local is_dir = vim.fn.isdirectory(path) == 1
+
+                -- if neovim is opened with an argument and that argument is a path pointing to a dir,
+                -- it will open a new buffer named as the last segment of the path (the last directory's name).
+                -- if we close this buffer, [No Name] buffer will be only left so that any new buffer can replace it,
+                -- and there will be no need to close it manualy 
+                if is_dir then
+                    vim.cmd('bd')
+                end
 
                 -- if path to a file is given, strip filename from the path and get the containing dir
                 local dir_path = apth.utils.methods.basedir({ path = path })
