5f59f7bc1b771960b4597f202d235604b076ce48
apathoid <apathoid@tuta.io>
2024-04-29 16:17:51 +0300

    editor: extract filetypes to enums

---
+++ lua/enum.lua
@@ -0,0 +1,15 @@
+return {
+    ft = {
+        file_explorer = {
+            nvimtree = 'NvimTree',
+            neotree = 'neo-tree'
+        },
+        split_utility = {
+            toggleterm = 'toggleterm'
+        },
+        git_window = {
+            gitcommit = 'gitcommit',
+            gitrebase = 'gitrebase'
+        }
+    }
+}
--- lua/globals.lua
+++ lua/globals.lua
@@ -1,4 +1,4 @@
-_G.apth = {
-    config = require('config'),
-    utils = require('utils.init')
-}
+_G.apth = {}
+_G.apth.enum = require('enum')
+_G.apth.config = require('config')
+_G.apth.utils = require('utils')
